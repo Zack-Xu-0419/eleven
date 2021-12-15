0ccf6cf0b21c0c5c8713b88d000a734b7d7dfde4
Zack Xu <zackxu1904@icloud.com>
2021-12-15 20:01:51 +0000

    Finished and Tested Cards and Deck

--- Deck.java
+++ Deck.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 /**
  * The Deck class represents a shuffled deck of cards.
  * It provides several operations including
- *      initialize, shuffle, deal, and check if empty.
+ * initialize, shuffle, deal, and check if empty.
  */
 public class Deck {
 
     /**
      * cards contains all the cards in the deck.
      */
-    private List<Card> cards;
+    private Card[] cards;
 
     /**
      * size is the number of not-yet-dealt cards.
@@ -20,34 +20,44 @@ public class Deck {
      */
     private int size;
 
-
     /**
      * Creates a new <code>Deck</code> instance.<BR>
      * It pairs each element of ranks with each element of suits,
      * and produces one of the corresponding card.
-     * @param ranks is an array containing all of the card ranks.
-     * @param suits is an array containing all of the card suits.
+     * 
+     * @param ranks  is an array containing all of the card ranks.
+     * @param suits  is an array containing all of the card suits.
      * @param values is an array containing all of the card point values.
      */
     public Deck(String[] ranks, String[] suits, int[] values) {
+        cards = new Card[ranks.length];
+        this.size = ranks.length;
         /* *** TO BE IMPLEMENTED IN PART 2 *** */
+        for (int i = 0; i < ranks.length; i++) {
+            Card theCard = new Card(ranks[i], suits[i], values[i]);
+            cards[i] = theCard;
+        }
+        this.shuffle();
     }
-
 
     /**
      * Determines if this deck is empty (no undealt cards).
+     * 
      * @return true if this deck is empty, false otherwise.
      */
     public boolean isEmpty() {
         /* *** TO BE IMPLEMENTED IN PART 2 *** */
+        return (cards.length == 0);
     }
 
     /**
      * Accesses the number of undealt cards in this deck.
+     * 
      * @return the number of undealt cards in this deck.
      */
     public int size() {
         /* *** TO BE IMPLEMENTED IN PART 2 *** */
+        return size;
     }
 
     /**
@@ -61,15 +71,23 @@ public class Deck {
 
     /**
      * Deals a card from this deck.
+     * 
      * @return the card just dealt, or null if all the cards have been
      *         previously dealt.
      */
     public Card deal() {
         /* *** TO BE IMPLEMENTED IN PART 2 *** */
+        if (this.size != 0) {
+            this.size = this.size - 1;
+            return cards[size];
+        } else {
+            return null;
+        }
     }
 
     /**
      * Generates and returns a string representation of this deck.
+     * 
      * @return a string representation of this deck.
      */
     @Override
@@ -77,7 +95,7 @@ public class Deck {
         String rtn = "size = " + size + "\nUndealt cards: \n";
 
         for (int k = size - 1; k >= 0; k--) {
-            rtn = rtn + cards.get(k);
+            rtn = rtn + cards[k];
             if (k != 0) {
                 rtn = rtn + ", ";
             }
@@ -88,12 +106,12 @@ public class Deck {
         }
 
         rtn = rtn + "\nDealt cards: \n";
-        for (int k = cards.size() - 1; k >= size; k--) {
-            rtn = rtn + cards.get(k);
+        for (int k = cards.length - 1; k >= size; k--) {
+            rtn = rtn + cards[k];
             if (k != size) {
                 rtn = rtn + ", ";
             }
-            if ((k - cards.size()) % 2 == 0) {
+            if ((k - cards.length) % 2 == 0) {
                 // Insert carriage returns so entire deck is visible on console.
                 rtn = rtn + "\n";
             }
--- DeckTester.java
+++ DeckTester.java
@@ -3,11 +3,23 @@
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-	}
+    /**
+     * The main method in this class checks the Deck operations for consistency.
+     * 
+     * @param args is not used.
+     */
+    public static void main(String[] args) {
+        /* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+        String[] ranks = { "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
+        String[] suits = { "hearts", "clubs", "diamonds", "spades", "clubs", "hears", "clubs", "spades", "diamonds",
+                "diamonds", "club", "spades", "hearts" };
+        int[] values = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+        Deck testDeck = new Deck(ranks, suits, values);
+        System.out.println(testDeck.deal());
+        System.out.println(testDeck.deal());
+        System.out.println();
+        System.out.println(testDeck.toString());
+        System.out.println(testDeck.isEmpty());// False
+        System.out.println(testDeck.size()); // 11
+    }
 }
